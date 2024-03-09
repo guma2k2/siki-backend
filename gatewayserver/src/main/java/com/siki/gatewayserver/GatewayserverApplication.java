0485964e5d98a42dcd6d07f4e29caa4f4a39f77c
thuan <thuanngo30072002@gmail.com>
2024-03-09 09:39:07 +0700

    Initial commit

---
+++ gatewayserver/src/main/java/com/siki/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,13 @@
+package com.siki.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+}
