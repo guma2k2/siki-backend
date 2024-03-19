8e3737895e8e54e0765daeb887434f6f742e996b
thuan <thuanngo30072002@gmail.com>
2024-03-19 10:31:05 +0700

    media service

---
+++ media/src/main/java/com/siki/media/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.siki.media.utils;
+
+public class Constants {
+    public final class ERROR_CODE {
+        public static final String MEDIA_NOT_FOUND = "MEDIA_NOT_FOUND";
+        public static final String ACCESS_DENIED_ERROR_FORMAT = "%s: Client %s don't have access right for this resource";
+    }
+}
