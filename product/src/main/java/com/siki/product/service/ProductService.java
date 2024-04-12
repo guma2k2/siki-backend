46b9c9b30f53d1dac9050a4fc7206c59573dd0b0
guma2k2 <102044008+guma2k2@users.noreply.github.com>
2024-04-12 09:03:12 +0700

    Update ProductService.java

--- product/src/main/java/com/siki/product/service/ProductService.java
+++ product/src/main/java/com/siki/product/service/ProductService.java
@@ -5,7 +5,9 @@ import com.siki.product.dto.product.*;
 import java.util.List;
 
 public interface ProductService {
+    // Todo: search product by price(min, max), brand, by ratingNums
     void create(BaseProductPostDto baseProductPostDto);
+    
     BaseProductDto getById(Long productId);
 
     ProductVariantDto findProductVariantById(Long productId);
